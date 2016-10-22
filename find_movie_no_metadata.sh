4c6495eab9103b5998cfdcb49b553fd32a4d0113
Robert Newbould <robert.newbould@gmail.com>
2016-10-22 20:11:27 +0100

    Uppercase SQL keywords

--- find_movie_no_metadata.sh
+++ find_movie_no_metadata.sh
@@ -1,4 +1,4 @@
 #!/opt/bin/bash
 
-psql -d video_metadata -U postgres -c "select path from video_file v inner join mapper m on v.mapper_id=m.id and m.type='movie' left outer join movie e on m.id=e.mapper_id where e.mapper_id is null; ; \
+psql -d video_metadata -U postgres -c "SELECT path FROM video_file v INNER JOIN mapper m ON v.mapper_id=m.id AND m.type='movie' LEFT OUTER JOIN movie e ON m.id=e.mapper_id WHERE e.mapper_id IS NULL; ; \
 " | sed "1,2d; :a; \$d; N; 3,4ba; P; D"
