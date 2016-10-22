87a4ffacd2b2ba3a2d00cbe121a92ab9889349dd
Robert Newbould <robert.newbould@gmail.com>
2016-10-22 20:48:43 +0100

    Format SQL

--- find_tv_no_metadata.sh
+++ find_tv_no_metadata.sh
@@ -1,3 +1,8 @@
 #!/opt/bin/bash
 
-psql -d video_metadata -At -c "SELECT path FROM video_file v INNER JOIN mapper m ON v.mapper_id=m.id AND m.type='tvshow_episode' LEFT OUTER JOIN tvshow_episode e ON m.id=e.mapper_id WHERE e.mapper_id IS NULL;"
+psql -d video_metadata -At <<-SQL
+  SELECT path FROM video_file v
+  INNER JOIN mapper m ON v.mapper_id = m.id AND m.type = 'tvshow_episode'
+  LEFT OUTER JOIN tvshow_episode e ON m.id = e.mapper_id
+  WHERE e.mapper_id IS NULL
+SQL
