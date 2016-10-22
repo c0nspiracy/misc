0a9051be56f12a09269c313077de43cead314a33
Robert Newbould <robert.newbould@gmail.com>
2016-10-22 20:14:29 +0100

    Connect to the database as the default user

--- find_tv_no_metadata.sh
+++ find_tv_no_metadata.sh
@@ -1,4 +1,4 @@
 #!/opt/bin/bash
 
-psql -d video_metadata -U postgres -c "SELECT path FROM video_file v INNER JOIN mapper m ON v.mapper_id=m.id AND m.type='tvshow_episode' LEFT OUTER JOIN tvshow_episode e ON m.id=e.mapper_id WHERE e.mapper_id IS NULL; ; \
+psql -d video_metadata -c "SELECT path FROM video_file v INNER JOIN mapper m ON v.mapper_id=m.id AND m.type='tvshow_episode' LEFT OUTER JOIN tvshow_episode e ON m.id=e.mapper_id WHERE e.mapper_id IS NULL; ; \
 " | sed "1,2d; :a; \$d; N; 3,4ba; P; D"
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/opt/bin/bash
 
 
-psql -d video_metadata -U postgres -c "SELECT path FROM video_file v INNER JOIN mapper m ON v.mapper_id=m.id AND m.type='tvshow_episode' LEFT OUTER JOIN tvshow_episode e ON e.mapper_id=m.id WHERE e.mapper_id IS NULL; \
+psql -d video_metadata -c "SELECT path FROM video_file v INNER JOIN mapper m ON v.mapper_id=m.id AND m.type='tvshow_episode' LEFT OUTER JOIN tvshow_episode e ON e.mapper_id=m.id WHERE e.mapper_id IS NULL; \
 " | sed "1,2d; :a; \$d; N; 3,4ba; P; D" | while read a; do 
   echo $a;
   #/var/packages/VideoStation/target/bin/synovideoindex -t videometa -a "$a"; 
